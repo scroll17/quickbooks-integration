92f43628da6bdba64e8999f611f5b04d084f6785
Denis Z <zololotarenko.2015@gmail.com>
2021-10-06 19:02:33 +0300

    added views/intuit.ejs (with connect button); updated /integration routes

--- src/router/integrationRoute.js
+++ src/router/integrationRoute.js
@@ -1,5 +1,79 @@
+// external modules
 const express = require('express');
+// services
+const QuickBooksService = require('../services/quickBooks/QuickBooksService')
+// db
+const db = require('../../data/index')
 
 const router = express.Router();
+
+router.get('/callback', async (req, res) => {
+    console.info('GET /integration/callback')
+    console.debug('TRACE req.url', req.url)
+
+    const userId = req.cookies['userId']
+    if(!userId) {
+        throw new Error(`userId not exist`)
+    }
+
+    res.clearCookie('userId')
+    console.debug('userId = ', userId)
+
+    const oauthClient = QuickBooksService.getClient();
+
+    const authResponse = await oauthClient.createToken(req.url);
+    const authTokens = JSON.stringify(authResponse.getJson(), null, 2);
+    console.debug('TRACE auth token\n', authTokens)
+
+    db.data = (db.data ?? {}).users = {
+        [userId]: {
+            tokens: authResponse.getJson()
+        }
+    }
+    await db.write();
+    console.debug('DB: SAVED')
+
+    console.log('')
+    res.send({ status: 'OK' })
+})
+
+router.get('/:userId', (req, res) => {
+    console.info('GET /integration/:userId')
+    console.info('RENDER intuit.ejs')
+
+    const userId = req.params['userId']
+    if(!userId) {
+        throw new Error(`userId required in params!`)
+    }
+
+    console.debug('userId = ', userId)
+    console.log('')
+
+    res.render('intuit.ejs', {
+        port: process.env.PORT,
+        appCenter: QuickBooksService.Constants.APP_CENTER_BASE,
+        userId
+    })
+})
+
+router.get('/requestToken/:userId', (req, res) => {
+    console.info('GET /integration/requestToken/:userId')
+
+    const userId = req.params['userId']
+    if(!userId) {
+        throw new Error(`userId required in params!`)
+    }
+
+    console.debug('userId = ', userId)
+
+    const oauthClient = QuickBooksService.getClient();
+    const authUri = QuickBooksService.Auth.buildAuthUri(oauthClient);
+
+    console.info('REDIRECT TO ', authUri)
+    console.log('')
+
+    res.cookie('userId', userId)
+    res.redirect(authUri);
+})
 
 module.exports = router;
