d32b6eab1b1bf230c366b8f2519e1a58f85860a6
Denis Z <zololotarenko.2015@gmail.com>
2021-10-07 13:03:44 +0300

    init webhook router

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,12 @@ const express = require('express');
 // routes
 const integrationRoute = require('./integrationRoute')
 const functionalRoute = require('./functionalRoute')
+const webhookRoute = require('./webhookRoute')
 
 const router = express.Router();
 
 router.use('/integration', integrationRoute);
 router.use('/functional', functionalRoute);
+router.use('/webhook', webhookRoute);
 
 module.exports = router;
---
+++ src/router/webhookRoute.js
@@ -0,0 +1,11 @@
+// external modules
+const express = require('express');
+// services
+const QuickBooksService = require('../services/quickBooks/QuickBooksService')
+// db
+const db = require('../../data/index')
+
+const router = express.Router();
+
+
+module.exports = router;
